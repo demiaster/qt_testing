26abc2e67c5db888b10e4c26c39064d2551af791
demiaster <marta.celeste.feriani@gmail.com>
2018-07-03 20:41:33 +0000

    Moved count logic to API. Dialog takes API instance.

--- qt_test.py
+++ qt_test.py
@@ -2,10 +2,10 @@ import sys
 from Qt import QtCore, QtGui, QtWidgets
 
 class MyDialog(QtWidgets.QDialog):
-	def __init__(self):
+	def __init__(self, api):
 		super(MyDialog, self).__init__()
 
-		self.model = QtGui.QStandardItemModel()
+		self.api = api
 
 		self.lv = QtWidgets.QListView()
 		self.button = QtWidgets.QPushButton("Blah")
@@ -16,12 +16,18 @@ class MyDialog(QtWidgets.QDialog):
 		self.setLayout(lyt)
 
 
-		self.lv.setModel(self.model)
+		self.lv.setModel(self.api.model)
 		self.button.clicked.connect(self.on_button_pressed)
 
+	def on_button_pressed(self):
+		self.api.add_item()
+
+class MyApi(object):
+	def __init__(self):
+		self.model = QtGui.QStandardItemModel()
 		self.count = 0
 
-	def on_button_pressed(self):
+	def add_item(self):
 		self.count += 1
 		self.model.appendRow(
 			QtGui.QStandardItem(
@@ -29,9 +35,11 @@ class MyDialog(QtWidgets.QDialog):
 			)
 		)
 
+
 if __name__ == "__main__":
 
 	qApp = QtWidgets.QApplication(sys.argv)
-	md = MyDialog()
+	api = MyApi()
+	md = MyDialog(api)
 	md.show()
 	sys.exit(qApp.exec_())
