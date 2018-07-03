ecb8dc58d1d56b75ad1316206942715e8275ff0d
demiaster <marta.celeste.feriani@gmail.com>
2018-07-03 20:29:26 +0000

    Dialog Class with button and layout

--- qt_test.py
+++ qt_test.py
@@ -1,9 +1,19 @@
 import sys
 from Qt import QtCore, QtGui, QtWidgets
 
+class MyDialog(QtWidgets.QDialog):
+	def __init__(self):
+		super(MyDialog, self).__init__()
+
+		self.button = QtWidgets.QPushButton("Blah")
+
+		lyt = QtWidgets.QVBoxLayout()
+		lyt.addWidget(self.button)
+		self.setLayout(lyt)
+
 if __name__ == "__main__":
 
 	qApp = QtWidgets.QApplication(sys.argv)
-	d = QtWidgets.QDialog()
-	d.show()
+	md = MyDialog()
+	md.show()
 	sys.exit(qApp.exec_())
