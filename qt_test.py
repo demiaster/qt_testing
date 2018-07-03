24bc58b304d8f47c4c4d3e7cafb27b36dbd6efbc
demiaster <marta.celeste.feriani@gmail.com>
2018-07-03 20:35:13 +0000

    Added model and ListView. Button adds item

--- qt_test.py
+++ qt_test.py
@@ -5,16 +5,24 @@ class MyDialog(QtWidgets.QDialog):
 	def __init__(self):
 		super(MyDialog, self).__init__()
 
+		self.model = QtGui.QStandardItemModel()
+
+		self.lv = QtWidgets.QListView()
 		self.button = QtWidgets.QPushButton("Blah")
 
 		lyt = QtWidgets.QVBoxLayout()
+		lyt.addWidget(self.lv)
 		lyt.addWidget(self.button)
 		self.setLayout(lyt)
 
+
+		self.lv.setModel(self.model)
 		self.button.clicked.connect(self.on_button_pressed)
 
 	def on_button_pressed(self):
-		print "Someone Pressed the Button!"
+		self.model.appendRow(
+			QtGui.QStandardItem('Blah')
+		)
 
 if __name__ == "__main__":
 
