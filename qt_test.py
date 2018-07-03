663d40648fa0f0d1dc6c7f4a3c14dc10c4a80785
demiaster <marta.celeste.feriani@gmail.com>
2018-07-03 20:45:32 +0000

    Button text altered to 'Add Item'. Api add_item called before UI initialized. PEP8

--- qt_test.py
+++ qt_test.py
@@ -1,5 +1,6 @@
 import sys
-from Qt import QtCore, QtGui, QtWidgets
+from Qt import QtGui, QtWidgets
+
 
 class MyDialog(QtWidgets.QDialog):
 	def __init__(self, api):
@@ -8,19 +9,19 @@ class MyDialog(QtWidgets.QDialog):
 		self.api = api
 
 		self.lv = QtWidgets.QListView()
-		self.button = QtWidgets.QPushButton("Blah")
+		self.button = QtWidgets.QPushButton("Add Item")
 
 		lyt = QtWidgets.QVBoxLayout()
 		lyt.addWidget(self.lv)
 		lyt.addWidget(self.button)
 		self.setLayout(lyt)
 
-
 		self.lv.setModel(self.api.model)
 		self.button.clicked.connect(self.on_button_pressed)
 
 	def on_button_pressed(self):
 		self.api.add_item()
+
 
 class MyApi(object):
 	def __init__(self):
@@ -40,6 +41,10 @@ if __name__ == "__main__":
 
 	qApp = QtWidgets.QApplication(sys.argv)
 	api = MyApi()
+
+	# Something happens before UI
+	api.add_item()
+
 	md = MyDialog(api)
 	md.show()
 	sys.exit(qApp.exec_())
